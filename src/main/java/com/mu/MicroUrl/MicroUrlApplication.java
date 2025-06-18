ee2d94146ee34c14720c529dda1b1b372bf2065a
ThiagoVini-S <th.vinialmeida@gmail.com>
2025-06-18 10:33:55 -0300

    Feat: First Commit

---
+++ src/main/java/com/mu/MicroUrl/MicroUrlApplication.java
@@ -0,0 +1,13 @@
+package com.mu.MicroUrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroUrlApplication.class, args);
+	}
+
+}
