f834fa2c28885b1f5e6c18c524b57d0df6e79d7a
ThiagoVini-S <th.vinialmeida@gmail.com>
2025-06-23 07:18:25 -0300

    Feat: Creating repository

---
+++ src/main/java/com/mu/MicroUrl/Infrastructure/UrlRepository.java
@@ -0,0 +1,8 @@
+﻿package com.mu.MicroUrl.Infrastructure;
+
+import com.mu.MicroUrl.Domain.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UrlRepository extends JpaRepository<Url, UUID>{ }
