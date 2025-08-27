800e4587a5af0d1c9cdfcc892edafe33149e6794
ThiagoVini-S <th.vinialmeida@gmail.com>
2025-08-27 11:00:42 -0300

    Feat: Adding Cors Config

---
+++ src/main/java/com/mu/MicroUrl/Configurations/WebConfig.java
@@ -0,0 +1,25 @@
+﻿package com.mu.MicroUrl.Configurations;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    @Value("${app.domain}")
+    private String appDomain;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a configuração a todos os endpoints ("/**")
+                .allowedOrigins(appDomain) // Permite origens específicas
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permite os métodos HTTP
+                .allowedHeaders("*") // Permite todos os cabeçalhos
+                .allowCredentials(true); // Permite o envio de credenciais (cookies, etc.)
+    }
+}
